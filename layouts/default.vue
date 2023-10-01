8bc5f732aca850dd88a632dd88b02d685fed4623
Csaba Verebélyi <csaba.verebelyi@gmail.com>
2023-10-01 08:45:21 +0200

    fix: full screen layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,15 @@
+<script setup lang="ts">
+useHead({
+  bodyAttrs: {
+    class: 'min-h-screen'
+  },
+})
+</script>
+
 <template>
-  <div>
+  <div class="min-h-screen flex flex-col bg-white dark:bg-gray-900">
     <AppHeader />
-    <main>
+    <main class="flex-grow">
       <section class="bg-white dark:bg-gray-900">
         <slot />
       </section>
