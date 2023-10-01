ec9f897b91c6c46e4a3fb2f0fb11ecb5f995e731
Csaba Verebélyi <csaba.verebelyi@gmail.com>
2023-10-01 09:08:08 +0200

    fix: use NuxtLink for navigation

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
       <h1
         class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white"
       >
-        Demo Page For Nuxt.js
+        My Blog With Nuxt.js
       </h1>
       <p
         class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400"
@@ -31,12 +31,6 @@
             clip-rule="evenodd"
           ></path>
         </svg>
-      </a>
-      <a
-        href="#"
-        class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-      >
-        Speak to Sales
       </a>
     </div>
     <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
