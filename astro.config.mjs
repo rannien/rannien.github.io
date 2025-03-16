8ed97f3610682fad0f057cab7718419c1879f128
Csaba Verebélyi <csaba.verebelyi@gmail.com>
2025-03-16 20:03:52 +0100

    feat: add GitHub Actions workflow for deploying to GitHub Pages and set site URL in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://rannien.github.io',
   vite: {
     plugins: [tailwindcss()]
   }
