cb1ce72ec30c1669372af3215c03d2dc9f5eabe6
Csaba Verebélyi <verebelyi.csaba@zengo.eu>
2026-07-25 22:35:45 +0200

    feat: :sparkles: upgrade to astro 7 and add blog with content collections

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,39 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig, fontProviders } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://rannien.github.io',
-  vite: {
-    plugins: [tailwindcss()]
-  }
+	site: 'https://rannien.github.io',
+	integrations: [mdx(), sitemap()],
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	fonts: [
+		{
+			provider: fontProviders.local(),
+			name: 'Atkinson',
+			cssVariable: '--font-atkinson',
+			fallbacks: ['sans-serif'],
+			options: {
+				variants: [
+					{
+						src: ['./src/assets/fonts/atkinson-regular.woff'],
+						weight: 400,
+						style: 'normal',
+						display: 'swap',
+					},
+					{
+						src: ['./src/assets/fonts/atkinson-bold.woff'],
+						weight: 700,
+						style: 'normal',
+						display: 'swap',
+					},
+				],
+			},
+		},
+	],
 });
---
+++ src/content/blog/first-post.md
@@ -0,0 +1,8 @@
+---
+title: 'First post'
+description: 'Lorem ipsum dolor sit amet'
+pubDate: 'Jul 08 2022'
+heroImage: '../../assets/blog-placeholder-3.jpg'
+---
+
+This is my first blog bost using Astro 7.x.
